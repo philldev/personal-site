9249e2b2651f417283db2124091680861d850ec5
Deddy Wolley <deddywolley@gmail.com>
2025-03-30 18:06:26 +0800

    add bg color

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,10 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          `${geistSans.variable} ${geistMono.variable} antialiased min-h-screen text-foreground`,
+          "bg-gradient-to-b from-slate-950/20 to-amber-950/20",
+        )}
       >
+        <header className="h-16"></header>
         {children}
       </body>
     </html>
